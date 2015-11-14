2a7d215b3500ce09629dfaaf6d78b78b579d479b
swe <samswe17@yahoo.com>
2015-11-14 14:48:50 -0800

    initial commit

---
+++ Cons.java
@@ -0,0 +1,11 @@
+class Cons
+{
+public Cons(int num,String str)
+{
+System.out.println("This is parameterised Constructor");
+}
+public static void main(String[] args)
+{
+	Cons a = new Cons(12,"sss");
+}
+}
